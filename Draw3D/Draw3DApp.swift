13a66098acd242b3696846c0698fd4bca1ef31ba
Jan Willem Brands <jan.willem.brands@gmail.com>
2022-10-26 21:14:05 +0200

    Initial Commit

---
+++ Draw3D/Draw3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Draw3DApp.swift
+//  Draw3D
+//
+//  Created by Johannes Brands on 26/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Draw3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
