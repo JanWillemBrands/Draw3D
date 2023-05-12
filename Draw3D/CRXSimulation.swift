10992112ee25de02ab338b25ae6400983c86690e
Jan Willem Brands <jan.willem.brands@gmail.com>
2023-05-12 23:33:46 +0200

    added joint/pivot nodes

--- Draw3D/CRXSimulation.swift
+++ Draw3D/CRXSimulation.swift
@@ -12,125 +12,82 @@ struct CRXSimulation: View {
     
     let crxScene = SCNScene(named: "SceneKit Asset Catalog.scnassets/3D_CRX-10iA_v02.scn")!
     
-    var base: SCNNode {
-        return crxScene.rootNode.childNode(withName: "base", recursively: true)!
-    }
+    let base: SCNNode
+    let shoulder: SCNNode
+    let elbow: SCNNode
+    let wrist1: SCNNode
+    let wrist2: SCNNode
+    let wrist3: SCNNode
+    let flange: SCNNode
     
-    var shoulder: SCNNode {
-        let node = crxScene.rootNode.childNode(withName: "shoulder", recursively: true)!
-        node.enumerateHierarchy { child, stop in
+//    let ikConstraint: SCNIKConstraint
+    
+    @State var angle: Float = 0
+    
+    init() {
+        base = crxScene.rootNode.childNode(withName: "base", recursively: true)!
+
+        shoulder = crxScene.rootNode.childNode(withName: "shoulder", recursively: true)!
+        shoulder.constraints = [xRotationConstraint]
+        
+        elbow = crxScene.rootNode.childNode(withName: "elbow", recursively: true)!
+        elbow.constraints = [xRotationConstraint]
+
+        wrist1 = crxScene.rootNode.childNode(withName: "wrist1", recursively: true)!
+        wrist1.constraints = [xRotationConstraint]
+
+        wrist2 = crxScene.rootNode.childNode(withName: "wrist2", recursively: true)!
+        wrist2.constraints = [xRotationConstraint]
+
+        wrist3 = crxScene.rootNode.childNode(withName: "wrist3", recursively: true)!
+        wrist3.constraints = [xRotationConstraint]
+
+        flange = crxScene.rootNode.childNode(withName: "flange", recursively: true)!
+        
+        flange.enumerateHierarchy { child, stop in
             if let materials = child.geometry?.materials {
                 for m in materials {
                     m.diffuse.contents = UIColor.red
                 }
             }
         }
-        node.constraints = [zRotationConstraint]
-        return node
-    }
-    
-    var elbow: SCNNode {
-        let node = crxScene.rootNode.childNode(withName: "elbow", recursively: true)!
-        node.enumerateHierarchy { child, stop in
-            if let materials = child.geometry?.materials {
-                for m in materials {
-                    m.diffuse.contents = UIColor.red
-                }
-            }
-        }
-        node.constraints = [yRotationConstraint]
-        return node
-    }
-    
-    var wrist1: SCNNode {
-        let node = crxScene.rootNode.childNode(withName: "wrist1", recursively: true)!
-        node.enumerateHierarchy { child, stop in
-            if let materials = child.geometry?.materials {
-                for m in materials {
-                    m.diffuse.contents = UIColor.red
-                }
-            }
-        }
-        node.constraints = [yRotationConstraint]
-        return node
-    }
-    
-    var wrist2: SCNNode {
-        let node = crxScene.rootNode.childNode(withName: "wrist2", recursively: true)!
-        node.enumerateHierarchy { child, stop in
-            if let materials = child.geometry?.materials {
-                for m in materials {
-                    m.diffuse.contents = UIColor.red
-                }
-            }
-        }
-        node.constraints = [xRotationConstraint]
-        return node
-    }
-    
-    var wrist3: SCNNode {
-        let node = crxScene.rootNode.childNode(withName: "wrist3", recursively: true)!
-        node.enumerateHierarchy { child, stop in
-            if let materials = child.geometry?.materials {
-                for m in materials {
-                    m.diffuse.contents = UIColor.red
-                }
-            }
-        }
-        node.constraints = [yRotationConstraint]
-        return node
-    }
-    
-    var flange: SCNNode {
-        let node = crxScene.rootNode.childNode(withName: "flange", recursively: true)!
-        node.enumerateHierarchy { child, stop in
-            if let materials = child.geometry?.materials {
-                for m in materials {
-                    m.diffuse.contents = UIColor.red
-                }
-            }
-        }
-        node.constraints = [xRotationConstraint]
-        return node
+                
+//        ikConstraint = SCNIKConstraint.inverseKinematicsConstraint(chainRootNode: base)
+//        ikConstraint.targetPosition = flange.worldPosition
+//        flange.constraints = [ikConstraint, xRotationConstraint]
+
     }
     
     let xRotationConstraint = SCNTransformConstraint(inWorldSpace: false) { node, matrix in
-//        if node.presentation.simdRotation.sum() > 0 {
-            node.eulerAngles.y = 0
-            node.eulerAngles.z = 0
-//        }
+        node.eulerAngles.y = 0
+        node.eulerAngles.z = 0
         return node.transform
     }
                                                                           
     let yRotationConstraint = SCNTransformConstraint(inWorldSpace: false) { node, matrix in
-            node.eulerAngles.x = 0
-            node.eulerAngles.z = 0
+        node.eulerAngles.x = 0
+        node.eulerAngles.z = 0
         return node.transform
     }
-                                                                          
+    
     let zRotationConstraint = SCNTransformConstraint(inWorldSpace: false) { node, matrix in
-            node.eulerAngles.x = 0
-            node.eulerAngles.y = 0
+        node.eulerAngles.x = 0
+        node.eulerAngles.y = 0
         return node.transform
     }
-                                                                          
-//        constraint = SCNIKConstraint.inverseKinematicsConstraint(chainRootNode: base)
-////        aimConstraint.setMaxAllowedRotationAngle(90, forJoint: nozzleNode)
-//        flange.constraints = [constraint]
     
     var body: some View {
         VStack {
+            Text(angle.description)
             Button("move") {
-                flange.simdRotation = flange.simdRotation + SIMD4(0.01, 0.01, 0.01, 0.5)
-                flange.simdPosition = SIMD3(-0.1, -0.1, -0.1)
+//                let tp = flange.simdWorldPosition + SIMD3(0.1, 0.1, 0.1)
+//                ikConstraint.targetPosition = SCNVector3(tp)
+//                flange.simdRotation = flange.simdRotation + SIMD4(0.1, 0.1, 0.1, 0.1)
+//                flange.simdPosition = flange.simdPosition + SIMD3(0.1, 0.1, 0.1)
             }
             SceneView(scene: crxScene, options: [.autoenablesDefaultLighting, .allowsCameraControl])
                 .frame(width: 500, height: 500)
                 .border(.black)
-        }
-        .onAppear {
-            let constraint = SCNIKConstraint.inverseKinematicsConstraint(chainRootNode: shoulder)
-            flange.constraints = [constraint]
         }
     }
 }
